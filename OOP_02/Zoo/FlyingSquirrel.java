12fa85fa6c00ee86c022a7336fe43d1a72ae7658
Pavel <black_yaguar@mail.ru>
2023-04-15 11:17:38 +0500

    Добавление нового класса

---
+++ OOP_02/Zoo/FlyingSquirrel.java
@@ -0,0 +1,21 @@
+public class FlyingSquirrel extends Herbivore implements Flyable, Walkable{
+    public FlyingSquirrel(String name){
+        super(name);
+
+    }
+
+    @Override
+    public String speak() {
+       return null;
+    }
+
+    @Override
+    public int flySpeed(){
+        return 35;
+
+    }
+    @Override
+    public int runSpeed() {
+        return 18;
+    }
+    }
